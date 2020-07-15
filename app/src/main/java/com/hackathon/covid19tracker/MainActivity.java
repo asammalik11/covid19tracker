ac8d6a090b11da28f63374a0fcab0a34ae04fd3c
Muhammad Zaidi <Muhammad.Zaidi@utoronto.ca>
2020-07-15 09:52:48 -0400

    nearby bluetooth devices working

--- app/src/main/java/com/hackathon/covid19tracker/MainActivity.java
+++ app/src/main/java/com/hackathon/covid19tracker/MainActivity.java
@@ -2,13 +2,146 @@ package com.hackathon.covid19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayAdapter<String> arrayAdapter;
+    ArrayList<String> listItems = new ArrayList<String>();
+    int clickCounter=0;
+
+    BluetoothAdapter bluetoothAdapter;
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.dView);
+        arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listItems);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Device Name : Bluetooth Address \n"+itemValue , Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        listView.setAdapter(arrayAdapter);
+
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: called.");
+        super.onDestroy();
+//        bluetoothAdapter.cancelDiscovery();
+    }
+
+    // Broadcast Receiver for listing devices that are not yet paired
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            Log.d(TAG, "onReceive: ACTION FOUND.");
+
+            if (action.equals(BluetoothDevice.ACTION_FOUND)){
+                BluetoothDevice device = intent.getParcelableExtra (BluetoothDevice.EXTRA_DEVICE);
+                Log.d(TAG, "onReceive: " + device.getName() + " : " + device.getAddress());
+
+                listItems.add(device.getName() + ": " + device.getAddress());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    // searching for unpaired devices
+    public void searchDevices(View view) {
+
+        if(bluetoothAdapter == null){
+            // Show Alert
+            Toast.makeText(getApplicationContext(),
+                    "Bluetooth not supported" , Toast.LENGTH_LONG)
+                    .show();
+
+            return;
+        }
+
+        // Show Alert
+        Toast.makeText(getApplicationContext(),
+                "Searching" , Toast.LENGTH_LONG)
+                .show();
+
+        listItems.clear();
+        arrayAdapter.notifyDataSetChanged();
+
+        Log.d(TAG, "Searching for unpaired devices");
+
+
+        if(bluetoothAdapter.isDiscovering()){
+            bluetoothAdapter.cancelDiscovery();
+            Log.d(TAG, "Canceling discovery.");
+
+            //check BT permissions in manifest
+            checkBTPermissions();
+
+            bluetoothAdapter.startDiscovery();
+            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mBroadcastReceiver, discoverDevicesIntent);
+        }
+        if(!bluetoothAdapter.isDiscovering()){
+
+            //check BT permissions in manifest
+            checkBTPermissions();
+
+            bluetoothAdapter.startDiscovery();
+            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mBroadcastReceiver, discoverDevicesIntent);
+        }
+    }
+
+
+    // Only required for API 23+
+    private void checkBTPermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
+
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+            }
+        }
+    }
+
+
 }
